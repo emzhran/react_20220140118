c99b1ac8aa81e83edf1de918c83d5c84460cd429
emzhran <zachran750@gmail.com>
2024-06-22 02:48:47 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import gambar from "./assets/zachran.png";
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text">Mohammad Zachran Zachary</h1>
+    <img className="gambar" src={gambar} alt="gambar"/>
+    <h1 className="text">20220140118</h1>
+    <h1 className="text">Mahasiswa Teknologi Informasi UMY</h1>
+    </>
+  )
+}
+
+export default App
